f91a9567c2200519939e940e5c42c503de8f8814
Vlad Basoon <vbasoon@gmail.com>
2022-06-07 10:45:05 +0300

    error dateTimeFormats

---
+++ src/i18n copy.ts
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueI18n, { LocaleMessages } from "vue-i18n";
+
+Vue.use(VueI18n);
+
+function loadLocaleMessages(): LocaleMessages {
+  const locales = require.context(
+    "./locales",
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const messages: LocaleMessages = {};
+  locales.keys().forEach((key) => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key);
+    }
+  });
+  return messages;
+}
+
+const dateTimeFormats = {
+  en: {
+    short: {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    },
+    long: {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      weekday: "long",
+      hour: "numeric",
+      minute: "numeric",
+    },
+  },
+};
+
+export default new VueI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages: loadLocaleMessages(),
+  //dateTimeFormats,
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,26 @@
   <div class="home">
     <section class="section">
       <h1>This is a Home Page</h1>
+      <p>{{ $t("message") }}</p>
+      <p>{{ $t("hello", { msg: "hello" }) }}</p>
+      <p>{{ $t("hello0", ["Моє ім'я", "hello"]) }}</p>
+      <p>{{ $tc("car", 1) }}</p>
+      <p>{{ $tc("car", 2) }}</p>
+      <p>{{ $tc("apple", 0) }}</p>
+      <p>{{ $tc("apple", 1) }}</p>
+      <p>{{ $tc("apple", 10, { count: testCount }) }}</p>
+      <p>{{ $d(new Date(), "short") }}</p>
     </section>
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
 <script>
-export default {
-  name: "HomeView",
-  components: {},
-};
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class Home extends Vue {
+  testCount = 20;
+}
 </script>
 
 <style lang="scss" scoped>
