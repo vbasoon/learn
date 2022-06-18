3b913baefc60dd485bbd869b421e8bf717148eda
vbasoon <vbasoon@gmail.com>
2022-06-18 12:12:45 +0300

    animation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
     <HeaderComponent
       v-if="$route.name !== 'Login' && $route.name !== 'page404'"
     />
-    <el-main> <router-view></router-view></el-main>
+    <el-main>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </el-main>
 
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <FooterComponent
@@ -13,7 +17,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import HeaderComponent from "@/layout/HeaderComponent.vue";
 import FooterComponent from "@/layout/FooterComponent.vue";
 import "@/assets/styles/global.scss";
@@ -30,6 +34,38 @@ export default {
 
 <style>
 .el-main {
+  overflow: hidden;
   padding-top: 80px;
+}
+
+.fade-enter-active {
+  animation: slidePageIn 3s forwards;
+  animation-timing-function: cubic-bezier(0.8, -0.49, 0.36, 1);
+}
+
+.fade-leave-active {
+  animation: slidePageDown 1s forwards;
+  animation-timing-function: cubic-bezier(0.8, -0.49, 0.36, 1);
+}
+
+@keyframes slidePageIn {
+  from {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+
+@keyframes slidePageDown {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0.1;
+    transform: translateY(100%);
+  }
 }
 </style>
